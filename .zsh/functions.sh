10b14457099510bab305c7c15b5c300259623d4e
Marc Qualie <marc@marcqualie.com>
2016-09-24 02:10:21 +0100

    Create functions to quickly dump/restore postgresql databases

--- .zsh/functions.sh
+++ .zsh/functions.sh
@@ -2,6 +2,36 @@
 
 
 
+db-dump() {
+  DB_DIR="/Users/$USER/iCloud Drive/Database Backups"
+  DB_NAME=$1
+  DB_FILE="$DB_DIR/$DB_NAME-$(date +%y%m%d).dump"
+  if [ -e $DB_FILE ]; then
+    echo "\033[31mBackup file ${DB_FILE} already exists"
+    return 127
+  fi
+  COMMAND="pg_dump -Fc --no-acl --no-owner -h localhost -U $USER $DB_NAME > \"$DB_FILE\""
+  echo $COMMAND
+  zsh -c "$COMMAND"
+  cp $DB_FILE $DB_DIR/$DB_NAME-latest.dump
+}
+
+db-restore() {
+  DB_DIR="/Users/$USER/iCloud Drive/Database Backups"
+  DB_NAME=$1
+  DB_FILE="$DB_DIR/$DB_NAME-latest.dump"
+  if [ ! -f "$DB_FILE" ]; then
+    echo "\033[31mBackup file $DB_FILE does not exist"
+    return 127
+  fi
+  COMMAND="pg_restore --verbose --clean --no-acl --no-owner -h localhost -U $USER -d $DB_NAME \"$DB_FILE\""
+  echo $COMMAND
+  zsh -c "psql -U $USER -c \"CREATE DATABASE $DB_NAME;\""
+  zsh -c "$COMMAND"
+}
+
+
+
 docker-init () {
   eval "$(docker-machine env fgdo)"
 }
