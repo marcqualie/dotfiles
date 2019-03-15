ddd9129d518f4a7e99f5b8c2f5cea72ed49dae53
Marc Qualie <marc@marcqualie.com>
2019-03-15 12:44:42 +0000

    Add new git aliases for committing and pushing

--- .zsh/aliases.sh
+++ .zsh/aliases.sh
@@ -6,6 +6,10 @@ alias gf="git fetch --all --prune"
 alias gfo="git fetch origin --prune"
 alias gb="git --no-pager branch -vv"
 alias gba="git --no-pager branch -avv"
+alias gcb="git checkout -b"
+alias gcm="git commit -m"
+alias gcam="git commit -a -m"
+alias gpoh="git push -u origin HEAD"
 
 # https://marcqualie.com/2015/08/remove-deleted-git-branches
 alias git-branch-cleanup="git --no-pager branch -vv | grep ': gone]' | awk '{print $1}' | xargs echo"
