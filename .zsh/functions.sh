04a11e9f0c407d787beb986c2f98abc76816bebb
Marc Qualie <marc@marcqualie.com>
2019-04-05 00:53:40 +0100

    Detect when entering symlink directories

--- .zsh/functions.sh
+++ .zsh/functions.sh
@@ -15,6 +15,12 @@ loadenv() {
   if [[ -f Gemfile ]]; then; +env ruby; fi
   if [[ -f package.json ]]; then; +env node; fi
   if [[ $(ls -1 | grep '.go$' | wc -l | awk '{$1=$1};1') != '0' ]]; then; +env go; fi
+
+  # Detect symlinked directories
+  # This comes up quite a lot when working with Go
+  SYMPATH=$(pwd)
+  REALPATH=$(pwd -P)
+  [[ "$SYMPATH" == "$REALPATH" ]] || echo "\x1b[33mYou have entered a symlink directory. Real path is \x1b[1m${REALPATH/$HOME/~}\x1b[0m"
 }
 
 
