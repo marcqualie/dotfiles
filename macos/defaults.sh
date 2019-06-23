bc291c8e9152e0c65ca71c28e7e9b5cd865bf40b
Marc Qualie <marc@marcqualie.com>
2019-06-23 11:33:29 +0100

    Improve macos defaults

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -1,21 +1,28 @@
 # Dock
+defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock show-recents -bool false
-defaults write com.apple.dock tilesize 48
+defaults write com.apple.dock show-process-indicators -bool true
+defaults write com.apple.dock tilesize 32
 killall Dock
 
 # Add a space to the Dock
-# defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="small-spacer-tile";}; killall Dock
+# defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="small-spacer-tile";}'; killall Dock
 
 # Keyboard
-defaults write NSGlobalDomain InitialKeyRepeat 13
+defaults write NSGlobalDomain InitialKeyRepeat 10
 defaults write NSGlobalDomain KeyRepeat 1
 
 # Security
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
+defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
 
 # Finder
 defaults write com.apple.finder AppleShowAllFiles -bool true
+defaults write com.apple.finder AppleShowAllFiles -bool true
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
+defaults write com.apple.finder ShowRecentTags -bool false
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-defaults write com.apple.dock show-process-indicators -bool true
-defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
+killall Finder
