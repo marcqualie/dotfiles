4c2b94203157b7dd886388efd4975dc51ed192d5
Marc Qualie <marc@marcqualie.com>
2021-07-15 12:58:48 +0100

    Load rbenv and nodenv in more contexts

--- .zsh/functions.sh
+++ .zsh/functions.sh
@@ -24,7 +24,11 @@ cdabs() {
 
 loadenv() {
   if [[ -f Gemfile ]]; then; +env ruby; fi
+  if [[ -f .ruby-version ]]; then; +env ruby; fi
+  if [[ -f backend/Gemfiile ]]; then; +env ruby; fi
   if [[ -f package.json ]]; then; +env node; fi
+  if [[ -f .node-version ]]; then; +env node; fi
+  if [[ -f frontend/package.json ]]; then; +env node; fi
   # if [[ $(ls -1 | grep '.go$' | wc -l | awk '{$1=$1};1') != '0' ]]; then; +env go; fi
 
   # Load everything when inside a vscode terminal
